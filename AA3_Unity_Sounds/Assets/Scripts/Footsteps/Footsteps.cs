69dd67f8b0d89759d645dbeed59fbd1961d45aea
David Martín <david.martin@enti.cat>
2021-05-29 19:27:51 +0200

    Caminando hace sonido pero raro como dessincronizado en la hierba
    Disminuir la duracion de los audios de la hierba

--- AA3_Unity_Sounds/Assets/Scripts/Footsteps/Footsteps.cs
+++ AA3_Unity_Sounds/Assets/Scripts/Footsteps/Footsteps.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FootSteps : MonoBehaviour
+public class Footsteps : MonoBehaviour
 {
     [SerializeField] private AudioClip[] concreteClips;
     [SerializeField] private AudioClip[] grassClips;
@@ -10,6 +10,12 @@ public class FootSteps : MonoBehaviour
     [SerializeField] private AudioClip[] woodClips;
 
     private AudioSource audioSource;
+
+    public float minVolume = 0.8f;
+    public float maxVolume = 1.0f;
+
+    public float minPitch = 0.9f;
+    public float maxPitch = 1.1f;
 
     private TerrainDetector terrainDetector;
     private void Awake()
@@ -22,23 +28,36 @@ public class FootSteps : MonoBehaviour
     private void Step()
     {
         AudioClip clip = GetRandomClip();
-        audioSource.PlayOneShot(clip);
+        if(clip!=null)
+        {
+            audioSource.clip = clip;
+            audioSource.volume = Random.Range(minVolume, maxVolume);
+            audioSource.pitch = Random.Range(minPitch, maxPitch);
+            audioSource.Play();
+        }
     }
 
     private AudioClip GetRandomClip()
     {
         int terrainTextureIndex = terrainDetector.GetActiveTerrainTextureIdx(transform.position);
-
-        switch (terrainTextureIndex)
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D))
         {
-            case 0:
-                return concreteClips.Length > 0 ? concreteClips[Random.Range(0, concreteClips.Length)] : null;
-            case 1:
-                return dirtClips.Length > 0 ? dirtClips[Random.Range(0, dirtClips.Length)] : null;
-            case 2:
-                return woodClips.Length > 0 ? woodClips[Random.Range(0, woodClips.Length)] : null;
-            default:
-                return grassClips.Length > 0 ? grassClips[Random.Range(0, grassClips.Length)] : null;
+            switch (terrainTextureIndex)
+            {
+                case 0:
+                    return grassClips.Length > 0 ? grassClips[Random.Range(0, grassClips.Length)] : null;
+                case 1:
+                    return dirtClips.Length > 0 ? dirtClips[Random.Range(0, dirtClips.Length)] : null;
+                case 2:
+                    return concreteClips.Length > 0 ? concreteClips[Random.Range(0, concreteClips.Length)] : null;
+                default:
+                    return woodClips.Length > 0 ? woodClips[Random.Range(0, woodClips.Length)] : null;
+            }
         }
+        else return null;
+    }
+    private void StopSteps()
+    {
+        audioSource.Stop();
     }
 }
