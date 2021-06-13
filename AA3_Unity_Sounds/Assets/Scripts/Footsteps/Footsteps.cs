b676878c7e71c9ca29ac8068e7959ec354d4015d
David Martín <david.martin@enti.cat>
2021-06-13 22:29:50 +0200

    Done boooiiiiis

--- AA3_Unity_Sounds/Assets/Scripts/Footsteps/Footsteps.cs
+++ AA3_Unity_Sounds/Assets/Scripts/Footsteps/Footsteps.cs
@@ -4,58 +4,78 @@ using UnityEngine;
 
 public class Footsteps : MonoBehaviour
 {
-    [SerializeField] private AudioClip[] concreteClips;
-    [SerializeField] private AudioClip[] grassClips;
-    [SerializeField] private AudioClip[] dirtClips;
-    [SerializeField] private AudioClip[] woodClips;
-    [SerializeField] private AudioClip[] jumpClips;
-    [SerializeField] private AudioClip pistacho;
+    //[SerializeField] private AudioClip[] concreteClips;
+    //[SerializeField] private AudioClip[] grassClips;
+    //[SerializeField] private AudioClip[] dirtClips;
+    //[SerializeField] private AudioClip[] woodClips;
+    //[SerializeField] private AudioClip[] jumpClips;
+    //[SerializeField] private AudioClip pistacho;
 
-    [SerializeField] private AudioSource audioSource;
-    [SerializeField] private AudioSource audioSourceAction;
-    private AudioClip clip;
+    //[SerializeField] private AudioSource audioSource;
+    //[SerializeField] private AudioSource audioSourceAction;
+    //private AudioClip clip;
 
-    public float minVolume = 0.8f;
-    public float maxVolume = 1.0f;
-    public float minVolumeJump = 0.7f;
-    public float maxVolumeJump = 1.0f;
+    //public float minVolume = 0.8f;
+    //public float maxVolume = 1.0f;
+    //public float minVolumeJump = 0.7f;
+    //public float maxVolumeJump = 1.0f;
 
-    public float minPitch = 0.9f;
-    public float maxPitch = 1.1f;
-    public float minPitchJump = 0.7f;
-    public float maxPitchJump = 1.3f;
+    //public float minPitch = 0.9f;
+    //public float maxPitch = 1.1f;
+    //public float minPitchJump = 0.7f;
+    //public float maxPitchJump = 1.3f;
+    [FMODUnity.EventRef]
+    public string fmodEventPathFS = "";
+    [FMODUnity.EventRef]
+    public string fmodEventPathJump = "";
+    [FMODUnity.EventRef]
+    public string fmodEventPathPistacho = "";
 
+    private FMOD.Studio.EventInstance footstepsInstance;
+    private FMOD.Studio.EventInstance jumpInstance;
+    private FMOD.Studio.EventInstance pistachoInstance;
     private bool jumping;
     private bool speaking;
+
+    private int iSurface = 0;
     
     [SerializeField] private float totalCD;
 
     private TerrainDetector terrainDetector;
+
     private void Awake()
     {
         terrainDetector = new TerrainDetector();
         jumping = false;
         speaking = false;
-    }
 
+    }
+    private void Start()
+    {
+        footstepsInstance = FMODUnity.RuntimeManager.CreateInstance(fmodEventPathFS);
+        jumpInstance = FMODUnity.RuntimeManager.CreateInstance(fmodEventPathJump);
+        pistachoInstance = FMODUnity.RuntimeManager.CreateInstance(fmodEventPathPistacho);
+    }
     private void Update()
     {
         if(Input.GetKeyDown(KeyCode.Space) && !jumping)
         {
             jumping = true;
             speaking = false;
-            audioSourceAction.clip = jumpClips[Random.Range(0, jumpClips.Length)];
-            audioSourceAction.volume = Random.Range(minVolumeJump, maxVolumeJump);
-            audioSourceAction.pitch = Random.Range(minPitchJump, maxPitchJump);
-            audioSourceAction.Play();
+            //audioSourceAction.clip = jumpClips[Random.Range(0, jumpClips.Length)];
+            //audioSourceAction.volume = Random.Range(minVolumeJump, maxVolumeJump);
+            //audioSourceAction.pitch = Random.Range(minPitchJump, maxPitchJump);
+            //audioSourceAction.Play();
+            jumpInstance.start();
         }
         if (Input.GetKeyDown(KeyCode.E) && !speaking)
         {
             speaking = true;
-            audioSourceAction.clip = pistacho;
-            audioSourceAction.volume = Random.Range(minVolume, maxVolume);
-            audioSourceAction.pitch = Random.Range(minPitch, maxPitch);
-            audioSourceAction.Play();
+            //audioSourceAction.clip = pistacho;
+            //audioSourceAction.volume = Random.Range(minVolume, maxVolume);
+            //audioSourceAction.pitch = Random.Range(minPitch, maxPitch);
+            //audioSourceAction.Play();
+            pistachoInstance.start();
             StartCoroutine(Speak());
         }
     }
@@ -67,29 +87,34 @@ public class Footsteps : MonoBehaviour
     //Step is an event from the Animation itself, everytime the animation fires "Step" , a Clip gets played
     private void Step()
     {
-        if(clip!=null && !jumping)
+        if(!jumping)
         {
-            audioSource.clip = clip;
-            audioSource.volume = Random.Range(minVolume, maxVolume);
-            audioSource.pitch = Random.Range(minPitch, maxPitch);
+            //audioSource.clip = clip;
+            //audioSource.volume = Random.Range(minVolume, maxVolume);
+            //audioSource.pitch = Random.Range(minPitch, maxPitch);
             if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D))
-                audioSource.Play();
+                footstepsInstance.start();
+                //audioSource.Play();
         }
     }
 
-    private AudioClip GetRandomClip()
+    private void GetRandomClip()
     {
         int terrainTextureIndex = terrainDetector.GetActiveTerrainTextureIdx(transform.position);
         switch (terrainTextureIndex)
         {
             case 0:
-                return grassClips.Length > 0 ? grassClips[Random.Range(0, grassClips.Length)] : null;
+                iSurface = 1;
+                break;
             case 1:
-                return dirtClips.Length > 0 ? dirtClips[Random.Range(0, dirtClips.Length)] : null;
+                iSurface = 0;
+                break;
             case 2:
-                return concreteClips.Length > 0 ? concreteClips[Random.Range(0, concreteClips.Length)] : null;
+                iSurface = 2;
+                break;
             default:
-                return woodClips.Length > 0 ? woodClips[Random.Range(0, woodClips.Length)] : null;
+                iSurface = 3;
+                break;
         }
     }
 
@@ -100,29 +125,30 @@ public class Footsteps : MonoBehaviour
             switch (hit.gameObject.tag)
             {
                 case "Stone":
-                    clip = concreteClips[Random.Range(0, dirtClips.Length)];
+                    iSurface = 2;
                     break;
                 default:
-                    clip = woodClips[Random.Range(0, woodClips.Length)];
+                    iSurface = 3;
                     break;
             }
             //activeSurfaceGroup = surfaceGroupArray[iSurface];
         }
         else
         {
-            clip = GetRandomClip();
+            GetRandomClip();
         }
-
+        footstepsInstance.setParameterByName("Parameter 1", iSurface);
         jumping = false;
     }
 
     private void OnCollisionStay(Collision collision)
     {
         if (collision.gameObject.tag == "Terrain")
-            clip = GetRandomClip();
+           GetRandomClip();
     }
     private void StopSteps()
     {
-        audioSource.Stop();
+        // audioSource.Stop();
+        //footstepsInstance.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
     }
 }
