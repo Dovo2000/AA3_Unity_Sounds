ee76f947baeacc1704f6256b6d05561479b33c0d
Victorín <victorgarcinuno@gmail.com>
2021-06-13 21:03:58 +0200

    Reverb and 3D ship sound

--- AA3_Unity_Sounds/Assets/RandomizeSound.cs
+++ AA3_Unity_Sounds/Assets/RandomizeSound.cs
@@ -1,41 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using FMODUnity;
 public class RandomizeSound : MonoBehaviour
 {
-
-        public AudioSource source;
 
         public float minWaitBetweenPlays = 2.0f;
         public float maxWaitBetweenPlays = 8.0f;
         public float waitTimeCountdown = 0.0f;
 
-
-        public float pitchMin = 0.8f;
-        public float pitchMax = 1.2f;
-
-        public float volMin = 0.1f;
-        public float volMax = 0.5f;
-
-
-
         void Start()
         {
-            source = GetComponent<AudioSource>();
-            StartCoroutine(RandomPlayBack(minWaitBetweenPlays, maxWaitBetweenPlays, waitTimeCountdown, pitchMin, pitchMax, volMin, volMax, source));
+            StartCoroutine(RandomPlayBack(minWaitBetweenPlays, maxWaitBetweenPlays, waitTimeCountdown));
         }
 
-        static IEnumerator RandomPlayBack(float minWaitBetweenPlays, float maxWaitBetweenPlays, float waitTimeCountdown, float pitchMin, float pitchMax, float volMin, float volMax, AudioSource source)
+        static IEnumerator RandomPlayBack(float minWaitBetweenPlays, float maxWaitBetweenPlays, float waitTimeCountdown)
         {
-            while (true)
-            {
-                waitTimeCountdown = Random.Range(minWaitBetweenPlays, maxWaitBetweenPlays);
-                source.pitch = Random.Range(pitchMin, pitchMax);
-                source.volume = Random.Range(volMin, volMax);
-                source.Play();
-                yield return new WaitForSeconds(waitTimeCountdown);
-            }
+        while (true)
+        {
+            FMODUnity.RuntimeManager.PlayOneShot("event:/Sound FX/3D FX/CreakWoodShip");
+            Debug.Log("se deberia reproducir"); 
+            waitTimeCountdown = Random.Range(minWaitBetweenPlays, maxWaitBetweenPlays);
+            yield return new WaitForSeconds(waitTimeCountdown);
+        }
+
         }
 
 }
---
+++ AA3_Unity_Sounds/Assets/SnapshotChange.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnapshotChange : MonoBehaviour
+{
+
+    FMOD.Studio.EventInstance reverbSnap;
+    // Start is called before the first frame update
+    void Start()
+    {
+        reverbSnap = FMODUnity.RuntimeManager.CreateInstance("snapshot:/ReverbChurch");
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        reverbSnap.start();
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        reverbSnap.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
